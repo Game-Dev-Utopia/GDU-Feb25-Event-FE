cb4a39ca76476efbf985a7c331daff97f9e5c583
Mayuri Kolhe <77161474+MayuriKolhe-2003@users.noreply.github.com>
2025-02-03 09:15:50 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 const instance = axios.create({
   //  baseURL: "http://localhost:8080",
- baseURL: "https://gdu-feb25-event-be.onrender.com/api/v1",
+ baseURL: "https://gdu-feb25-event-be.onrender.com",
   timeout: 50000,
   headers: {
     "Content-Type": "application/json",
